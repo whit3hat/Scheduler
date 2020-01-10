94f14f58e9e4b977728fa916101e8a0c3e7dee01
Spencer Wood <spencer.wood@me.com>
2020-01-09 20:10:10 -0600

    replaced HTML with orginial to fix issues I'd created

---
+++ script.js
@@ -0,0 +1,21 @@
+//variables
+
+//variables for HTML connectors
+const hour = document.getElementsByClassName('#container');
+//const currentDay = document.getElementById('.currentDay');
+
+//Get current day 
+const currentDay = '2020-01-09';
+var dateFormat = 'll';
+const today = moment(currentDay).format(dateFormat);
+console.log(currentDay);
+
+//display current day 
+
+//change colors of hour blocks
+
+//input schedule information
+
+//store event information with local storage
+
+
